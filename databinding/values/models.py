ae8b2fa07073c269eec8ab082867355c28eacb0b
Andrew Godwin <andrew@aeracode.org>
2016-07-18 14:58:46 -0400

    Just use default serializing now

--- databinding/values/models.py
+++ databinding/values/models.py
@@ -13,21 +13,8 @@ class IntegerValueBinding(WebsocketBinding):
 
     model = IntegerValue
 
-    def serialize_data(self, instance):
-        return {"name": instance.name, "value": instance.value}
-
     def group_names(self, instance, action):
         return ["binding.values"]
 
     def has_permission(self, user, action, pk):
         return True
-
-    def create(self, data):
-        self.model.objects.create(name=data['name'], value=data['value'])
-
-    def update(self, instance, data):
-        if "name" in data:
-            instance.name = data['name']
-        if "value" in data:
-            instance.value = data['value']
-        instance.save()
